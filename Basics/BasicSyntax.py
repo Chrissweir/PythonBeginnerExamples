3d78bb7ee42acc0da64fce0310b8515f65f031bc
Christopher Weir <cweir@SIDERO.Athlone.Local>
2017-07-25 13:02:34 +0100

    Small comments on the basics

---
+++ Basics/BasicSyntax.py
@@ -0,0 +1,19 @@
+#Math Operators from Highest to Lowest Precedence
+"""Operator     Operation   Example     Evaluates to...
+
+    **          Exponent    2 ** 3          8
+    %     Modulus/remainder 22 % 8          6
+    //      Integer division 22 // 8        2
+    /           Division    22 / 8        2.75
+    *       Multiplication  3 * 5           15
+    -          Subtraction  5 - 2           3
+    +           Addition    2 + 2           4
+
+#Common Data Types
+    Data type           Examples
+    Integers    -2, -1, 0, 1, 2, 3, 4, 5
+
+    Floats  -1.25, -1.0, --0.5, 0.0, 0.5, 1.0, 1.25
+
+    Strings     'a', 'aa', 'aaa', 'Hello!', '11 cats'
+"""
